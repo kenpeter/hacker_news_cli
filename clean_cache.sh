83196d1eeb9c3e74c6414b3147be6821cdb72719
kenpeter <figo2476@hotmail.com>
2017-03-09 16:49:29 +1100

    able to read news

---
+++ clean_cache.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rm ./cache/*.json
---
+++ config.js
@@ -0,0 +1,5 @@
+//https://newsapi.org/v1/articles?source=hacker-news&sortBy=top&apiKey=3aff3caea1e94d179383179582c1e075
+
+module.exports = {
+  apiKey: '3aff3caea1e94d179383179582c1e075'
+};
